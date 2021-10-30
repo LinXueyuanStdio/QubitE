e1559d2eb5454077416e3e7ab48dd158d5258427
兮尘 <761516186@qq.com>
2021-10-30 12:11:44 +0800

    Update train.py

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ class MyExperiment(Experiment):
         data.load_cache(["train_triples_ids", "test_triples_ids", "valid_triples_ids", "all_triples_ids"])
         data.load_cache(["hr_t_train"])
         data.print(self.log)
-        self.store.save_scripts(["train_LinkPredict_BoolE.py", "BoolE.py"])
+        self.store.save_scripts(["train.py", "model.py", "QubitEmbedding.py"])
         max_relation_id = data.relation_count
 
         # 1. build train dataset
