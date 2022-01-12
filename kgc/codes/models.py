b8820e402d60f4ce5d7fcd8eaf66c474257b68ff
兮尘 <761516186@qq.com>
2022-01-12 18:31:19 +0800

    Update models.py

--- kgc/codes/models.py
+++ kgc/codes/models.py
@@ -7,7 +7,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
-from QubitEmbedding import QubitMult
 from data import BatchType, TestDataset
 
 
@@ -438,6 +437,146 @@ class Rotate3D(KGEModel):
 #         score = score.norm(dim=0, p=self.p)
 #         score = self.gamma.item() - score.sum(dim=2)
 #         return score
+
+class ComplexMult(nn.Module):
+    """
+    h = h_a + h_b i
+    r = r_a + r_b i
+    h * r = (h_a * r_a - h_b * r_b) + (h_a * r_b + h_b * r_a) i
+
+    h in C^d
+    r in C^d
+    """
+
+    def __init__(self, norm_flag=False):
+        super(ComplexMult, self).__init__()
+        self.flag_hamilton_mul_norm = norm_flag
+
+    def forward(self, h, r):
+        h_a, h_b = h
+        r_a, r_b = r
+
+        if self.flag_hamilton_mul_norm:
+            # Normalize the relation to eliminate the scaling effect
+            r_norm = torch.sqrt(r_a ** 2 + r_b ** 2)
+            r_a = r_a / r_norm
+            r_b = r_b / r_norm
+        t_a = h_a * r_a - h_b * r_b
+        t_b = h_a * r_b + h_b * r_a
+        return t_a, t_b
+
+
+class ComplexAdd(nn.Module):
+    """
+    h = h_a + h_b i
+    r = r_a + r_b i
+    h + r = (h_a + r_a) + (h_b + r_b) i
+
+    h in C^d
+    r in C^d
+    """
+
+    def __init__(self):
+        super(ComplexAdd, self).__init__()
+
+    def forward(self, h, r):
+        h_a, h_b = h
+        r_a, r_b = r
+
+        t_a = h_a + r_a
+        t_b = h_b + r_b
+        return t_a, t_b
+
+
+class ComplexConjugate(nn.Module):
+    """
+    h = h_a + h_b i
+    ^h = h_a - h_b i
+
+    h in C^d
+    r in C^d
+    """
+
+    def __init__(self):
+        super(ComplexConjugate, self).__init__()
+
+    def forward(self, h):
+        h_a, h_b = h
+        return h_a, -h_b
+
+
+class ComplexSubstract(nn.Module):
+    """
+    h = h_a + h_b i
+    r = r_a + r_b i
+    h - r = (h_a - r_a) + (h_b - r_b) i
+
+    h in C^d
+    r in C^d
+    """
+
+    def __init__(self):
+        super(ComplexSubstract, self).__init__()
+
+    def forward(self, h, r):
+        h_a, h_b = h
+        r_a, r_b = r
+
+        t_a = h_a - r_a
+        t_b = h_b - r_b
+        return t_a, t_b
+
+
+class ComplexDiv(nn.Module):
+    """
+    h = h_a + h_b i
+    r = r_a + r_b i
+    h / r = [(h_a * r_a + h_b * r_b) / (r_a ^2 + r_b ^2)] + [(h_b * r_a - h_a * r_b) / (r_a ^2 + r_b ^2)] i
+
+    h in C^d
+    r in C^d
+    """
+
+    def __init__(self):
+        super(ComplexDiv, self).__init__()
+
+    def forward(self, h, r):
+        h_a, h_b = h
+        r_a, r_b = r
+
+        r_norm = torch.sqrt(r_a ** 2 + r_b ** 2)
+
+        t_a = (h_a * r_a + h_b * r_b) / r_norm
+        t_b = (h_b * r_a - h_a * r_b) / r_norm
+        return t_a, t_b
+
+class QubitMult(nn.Module):
+    """
+    U[r] = [[r_a, -^r_b],
+            [r_b, ^r_a ]]  ^ is conj
+    h = [h_a, h_b]
+
+    h_a, h_b in CP^d
+    r_a, r_b in CP^d
+
+    h * r = U[r] * h = [r_a * h_a + -^r_b * h_b, r_b * h_a + ^r_a * h_b]
+    """
+
+    def __init__(self, norm_flag=False):
+        super(QubitMult, self).__init__()
+        self.norm_flag = norm_flag
+        self.complex_mul = ComplexMult(False)
+        self.complex_add = ComplexAdd()
+        self.complex_sub = ComplexSubstract()
+        self.complex_div = ComplexDiv()
+        self.complex_conj = ComplexConjugate()
+
+    def forward(self, h, r):
+        h_a, h_b = h
+        r_a, r_b = r
+        a = self.complex_sub(self.complex_mul(h_a, r_a), self.complex_mul(h_b, self.complex_conj(r_b)))
+        b = self.complex_add(self.complex_mul(h_a, r_b), self.complex_mul(h_b, self.complex_conj(r_a)))
+        return a, b
 
 
 class QubitE(KGEModel):
